798c454519e9f6f4c2eadaf294b7e63afe5b3afd
Ricardo Klug <62856822+RicardoKlug02@users.noreply.github.com>
2026-09-07 10:44:45 -0300

    Atualização

--- src/backend/app/models/cliente.py
+++ src/backend/app/models/cliente.py
@@ -7,7 +7,7 @@ class Cliente(Base):
     id = Column(Integer, primary_key=True, autoincrement=True)
     razao_social = Column(String(150), nullable=False)
     nome_fantasia = Column(String(150))
-    cnpj_cpf = Column(String(18), unique=True)
+    cnpj_cpf = Column(String(64), unique=True)
     cep = Column(String(10))           
     grupo_economico = Column(String(100)) #Mapeado de "Rede de clientes"
     micro_regiao = Column(String(50))
